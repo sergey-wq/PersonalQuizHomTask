49479831adebd02466e2a6e7f3348fbef4411bcb
sergeykin <sergeykin1997@mail.ru>
2022-03-16 13:16:39 +0300

    feat: update data in result vc

--- PersonalQuiz/ViewControllers/ResultViewController.swift
+++ PersonalQuiz/ViewControllers/ResultViewController.swift
@@ -11,30 +11,34 @@ class ResultViewController: UIViewController {
     
     @IBOutlet private weak var whatIsAnimalLabel: UILabel!
     @IBOutlet private weak var yourHobbiesLabel: UILabel!
-
+    
     var answers: [Answer] = []
-
+    
     private var dictionaryAnswer: [Animal: Int] = [:]
-
-
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.hidesBackButton = true
         setAnswerInLabels()
     }
-
+    
     private func getDictionaryAnswer() -> [Dictionary<Animal, Int>.Element] {
         for answer in answers {
-            dictionaryAnswer.updateValue(answer.title.count, forKey: answer.animal)
+            if let animalCount = dictionaryAnswer[answer.animal] {
+                dictionaryAnswer.updateValue(animalCount + 1, forKey: answer.animal)
+            } else {
+                dictionaryAnswer[answer.animal] = 1
+            }
         }
-
-        let sortedDictionaryAnswer = dictionaryAnswer.sorted(by: { $0.value > $1.value })
+        
+        let sortedDictionaryAnswer = dictionaryAnswer.sorted { $0.value > $1.value }
         return sortedDictionaryAnswer
     }
-
+    
     private func setAnswerInLabels() {
         guard let emoji = getDictionaryAnswer().first?.key.rawValue else { return }
-
+        
         whatIsAnimalLabel.text = "Вы - \(emoji)!"
         yourHobbiesLabel.text = getDictionaryAnswer().first?.key.definition
     }
